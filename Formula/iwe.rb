1b39600b67c7998f87ad208f0b113d0675f3c646
Dmytro Halichenko <dmytrohalichenko@gmail.com>
2026-07-26 22:00:37 -0400

    Update iwe to 0.16.0

--- Formula/iwe.rb
+++ Formula/iwe.rb
@@ -1,23 +1,23 @@
 class Iwe < Formula
   desc "Markdown-based personal knowledge management tool for developers"
   homepage "https://iwe.md"
-  version "0.15.0"
+  version "0.16.0"
   license "Apache-2.0"
 
   on_macos do
     url "https://github.com/iwe-org/iwe/releases/download/iwe-v#{version}/iwe-v#{version}-universal-apple-darwin.tar.gz"
-    sha256 "1f49c411e1f23bc0a0b505cea09708e3919ec34c2e55e8c61f554c186bef38e9"
+    sha256 "745c70cb71b13b504f2644cc44178bc09131bdd86808eb3805d50ebded13c834"
   end
 
   on_linux do
     on_intel do
       url "https://github.com/iwe-org/iwe/releases/download/iwe-v#{version}/iwe-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "7028b2528f77426a3b519fea5b6757787b02c68083a488c6bbaf782d62c6135b"
+      sha256 "4d6995a9b670d488a87e6052f3644fcc675ed7900673c603a44f25d11e2024b1"
     end
 
     on_arm do
       url "https://github.com/iwe-org/iwe/releases/download/iwe-v#{version}/iwe-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "4bde27cead3bfceff5c64ec16fd1a2a2224ec0819a7ed7b4c41f055cb7813280"
+      sha256 "7139a055d3d9a19f165ee3bdf8e36980b8f9a2debd534173e8347d0a8ec71347"
     end
   end
 
